dbb6c801c4c8a948e7597035e3448854d74d179f
NerdsCorpx <86178816+NerdsCorp@users.noreply.github.com>
2025-10-11 00:22:07 -0700

    Update utils.py

--- backend/utils.py
+++ backend/utils.py
@@ -10,6 +10,8 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 # Hash a password
 # -----------------------------
 def get_password_hash(password: str) -> str:
+    # bcrypt has a 72-byte limit, truncate if longer
+    password = password[:72]
     return pwd_context.hash(password)
 
 # -----------------------------
