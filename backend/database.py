cd8abf6b2f6b8078ad5b1f74fd57dee2a8c1dd9d
NerdsCorpx <86178816+NerdsCorp@users.noreply.github.com>
2025-10-11 00:04:25 -0700

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,4 +1,3 @@
-```python
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -23,4 +22,3 @@ try:
 yield db
 finally:
 db.close()
-```
